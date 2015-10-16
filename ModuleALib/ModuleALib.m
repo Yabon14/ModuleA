5f2adeed2cd22eadb68ea173ae64a86f8d9e3d94
Yann BONAFONS <yann.bonafons@scmfrance.fr>
2015-10-16 17:12:47 +0200

    first commit

---
+++ ModuleALib/ModuleALib.m
@@ -0,0 +1,17 @@
+//
+//  ModuleALib.m
+//  ModuleALib
+//
+//  Created by Yann BONAFONS on 13/10/2015.
+//  Copyright © 2015 Yann BONAFONS. All rights reserved.
+//
+
+#import "ModuleALib.h"
+
+@implementation ModuleALib
+
++ (void) sayHelloFromModuleA{
+    NSLog(@"Hello from module A modified in the workspace!");
+}
+
+@end
